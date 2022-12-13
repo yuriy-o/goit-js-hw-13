b33e95330975b5698644513f6d21a6b4db260ca0
Yuriy <sharik.kiev@gmail.com>
2022-12-13 21:33:14 +0200

    upd

--- src/index2.js
+++ src/index2.js
@@ -2,21 +2,24 @@ async function onLoadMore() {
   newsApiService.incrementPage();
   // newsApiService
 
-  const data = await newsApiService.fetchImage();
-
   try {
-    ({ data: { hits, totalHits } }) => {
-      console.log(totalHits);
-      renderCard(hits);
-      console.log(newsApiService.page);
-      console.log(totalPages);
-      if (newsApiService.page === totalPages) {
-        refs.buttonLoad.classList.add('is-hidden');
-        Notify.failure(
-          'We are sorry, but you have reached the end of search results'
-        );
-      }
-    };
+    const data = await newsApiService.fetchImage();
+
+    const hits = data.hits;
+    console.log(hits);
+
+    const totalHits = data.totalHits;
+    console.log(totalHits);
+
+    renderCard(hits);
+    console.log(newsApiService.page);
+    console.log(totalPages);
+    if (newsApiService.page === totalPages) {
+      refs.buttonLoad.classList.add('is-hidden');
+      Notify.failure(
+        'We are sorry, but you have reached the end of search results'
+      );
+    }
   } catch (error) {
     console.log(error);
   }
