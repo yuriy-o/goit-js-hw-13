9881d2d10c4acdf13ad73d44212388c6bbdda8b8
Yuriy <yuriy.orekhov@gmail.com>
2022-12-15 23:08:29 +0200

    add handlebars

--- src/gallery-1.js
+++ src/gallery-1.js
@@ -28,7 +28,7 @@ export function createGallery(galleryItems) {
       </div>
       <div class="info">
         <p class="info-item"><b>Likes</b> ${likes}</p>
-        <p class="info-item"><b>Views</b> ${views}</p>
+        <p class="info-item"><b>Views</b> ${views}</p>npm i handlebars
         <p class="info-item"><b>Comments</b> ${comments}</p>
         <p class="info-item"><b>Downloads</b> ${downloads}</p>
       </div>
