ee4a1c60ed079deabe7d3fab1360fe0354971f5b
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2022-12-26 16:08:00 +0200

    change code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-async function onLoadMore() {
-  newsApiService.incrementPage();
-  // newsApiService
-        
-  await newsApiService
-    .fetchImage()
-    .then(({ data: { hits, totalHits } }) => {
-      console.log(totalHits);
-      renderCard(hits);
-      console.log(newsApiService.page);
-      console.log(totalPages);
-      if (newsApiService.page === totalPages) {
-        refs.buttonLoad.classList.add('is-hidden');
-        Notify.failure(
-          'We are sorry, but you have reached the end of search results'
-        );
-      }
-      
-    })
-    .catch(error => console.log(error));
+// async function onLoadMore() {
+//   newsApiService.incrementPage();
+//   // newsApiService
+
+//   await newsApiService
+//     .fetchImage()
+//     .then(({ data: { hits, totalHits } }) => {
+//       console.log(totalHits);
+//       renderCard(hits);
+//       console.log(newsApiService.page);
+//       console.log(totalPages);
+//       if (newsApiService.page === totalPages) {
+//         refs.buttonLoad.classList.add('is-hidden');
+//         Notify.failure(
+//           'We are sorry, but you have reached the end of search results'
+//         );
+//       }
+
+//     })
+//     .catch(error => console.log(error));
+
+//TODO треба почистити код в файлах цього репозиторію
