59505193e01938f9c5d7de409a46f1d08ff1b17b
yuriy-o <82809592+yuriy-o@users.noreply.github.com>
2022-12-26 23:01:48 +0000

    Deploying to gh-pages from @ yuriy-o/goit-js-hw-13@a14e638a04c27dd31c78de2ce6eaaa82faa58ba0 🚀

---
+++ index.5dfd9af1.js
@@ -0,0 +1,5 @@
+!function(){const e=["banana"].push("apple");console.log(e);
+//! Task 01-2
+//! Task 02
+let o=[["unshift",!1,1,2,3,"complex","nested"],[["iterate",1.3849,7,"8.4876","arbitrary","depth"],["loop","shift",6,7,1e3,"method","deep"]],[[["concat",!1,!0,"spread","array","deeper"]],[[["mutate",1327.98,"splice","slice","push","deepest"]]]]];console.log(o[2][0][0]),console.log(o[2][0][1][0]),console.log(o[2][0][1][1][0])}();
+//# sourceMappingURL=index.5dfd9af1.js.map
