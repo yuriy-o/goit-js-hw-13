f1f9b22b1156925f29d8071b0bdcd91b42dccbf4
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2022-12-26 15:39:26 +0200

    new tasks

--- src/index2.js
+++ src/index2.js
@@ -1,28 +1,63 @@
-async function onLoadMore() {
-  newsApiService.incrementPage();
-  // newsApiService
+// async function onLoadMore() {
+//   newsApiService.incrementPage();
+//   // newsApiService
 
-  try {
-    const data = await newsApiService.fetchImage();
+//   try {
+//     const data = await newsApiService.fetchImage();
 
-    const { hits } = data;
-    // const hits = data.hits;
-    console.log(hits);
+//     const { hits } = data;
+//     // const hits = data.hits;
+//     console.log(hits);
 
-    const { totalHits } = data;
-    // const totalHits = data.totalHits;
-    console.log(totalHits);
+//     const { totalHits } = data;
+//     // const totalHits = data.totalHits;
+//     console.log(totalHits);
 
-    renderCard(hits);
-    console.log(newsApiService.page);
-    console.log(totalPages);
-    if (newsApiService.page === totalPages) {
-      refs.buttonLoad.classList.add('is-hidden');
-      Notify.failure(
-        'We are sorry, but you have reached the end of search results'
-      );
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     renderCard(hits);
+//     console.log(newsApiService.page);
+//     console.log(totalPages);
+//     if (newsApiService.page === totalPages) {
+//       refs.buttonLoad.classList.add('is-hidden');
+//       Notify.failure(
+//         'We are sorry, but you have reached the end of search results'
+//       );
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+//! Task 01
+
+// function addToList(item, list) {
+//   return list.push(item);
+// }
+
+// const result = addToList('apple', ['banana']);
+// console.log(result); // 2
+
+// a) ['apple', 'banana'];
+// b) ['banana', 'apple']
+// c) true
+// d) 2
+// e) undefined
+
+//! Task 02
+// Змініть myNestedArray, використовуючи будь-яку комбінацію рядків , чисел і логічних значень для елементів даних,
+// щоб він мав рівно п’ять рівнів глибини(пам’ятайте, що крайній масив має рівень 1).
+// Десь на третьому рівні включите рядок deep, на четвертому рівні включите рядок deeper, а на п’ятому рівні включите рядок deepest.
+
+let myNestedArray = [
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  [
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth'],
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep'],
+  ],
+  [
+    [['concat', false, true, 'spread', 'array', 'deeper']],
+    [[['mutate', 1327.98, 'splice', 'slice', 'push', 'deepest']]],
+  ],
+];
+console.log(myNestedArray[2][0][0]);
+console.log(myNestedArray[2][0][1][0]);
+console.log(myNestedArray[2][0][1][1][0]);
